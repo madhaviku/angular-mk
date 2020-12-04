a5b769a1be646a058387c26ca3cb7860cc8ae29b
madhaviku <madhavi.kulkarni180796@gmail.com>
2020-12-04 17:39:49 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-mk
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-mk)
---
+++ src/app/cc-modelling.ts
@@ -0,0 +1,14 @@
+import {Component} from '@angular/core';
+
+/** @title Simple form field */
+@Component({
+  selector: 'cc-modelling',
+  templateUrl: 'cc-modelling.html',
+  styleUrls: ['./cc-modelling.css'],
+})
+export class CcModelling {}
+
+
+/**  Copyright 2020 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at http://angular.io/license */
