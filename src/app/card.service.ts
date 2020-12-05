5a38d874afa617cea6f9465f248324cfb30d70ae
madhaviku <madhavi.kulkarni180796@gmail.com>
2020-12-05 15:42:51 +0530

    3 files modified

--- src/app/card.service.ts
+++ src/app/card.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from "@angular/core";
 import { BehaviorSubject } from "rxjs/BehaviorSubject";
 import { Observable } from "rxjs/Observable";
+import { MatSnackBar } from "@angular/material/snack-bar";
 
 @Injectable()
 export class CardService {
+  constructor(private readonly snackbar: MatSnackBar) {}
   private cardDetails = new BehaviorSubject<any>([
     {
       cardNumber: "0000 0000 0000 0000",
@@ -15,6 +17,7 @@ export class CardService {
   ]);
 
   setpayment(data: any) {
+    this.snackbar.open("Payment completed successfully!", "Dismiss");
     this.cardDetails.next([data]);
   }
 
--- src/app/cc-modelling.ts
+++ src/app/cc-modelling.ts
@@ -1,6 +1,4 @@
 import { Component } from "@angular/core";
-import { CardService } from "./card.service";
-import { Router } from "@angular/router";
 
 /** @title Simple form field */
 @Component({
@@ -8,30 +6,4 @@ import { Router } from "@angular/router";
   templateUrl: "cc-modelling.html",
   styleUrls: ["./cc-modelling.css"]
 })
-export class CcModelling {
-  cardNumber: string;
-  cardName: string;
-  expiryDate: string;
-  securiyCode: string;
-  amount: string;
-
-  constructor(
-    private cardService: CardService,
-    private readonly router: Router
-  ) {}
-
-  submitData() {
-    const paymentData = {
-      cardNumber: this.cardNumber,
-      cardName: this.cardName,
-      expiryDate: this.expiryDate,
-      securiyCode: this.securiyCode,
-      amount: this.amount
-    };
-    this.cardService.setpayment(paymentData);
-  }
-
-  viewDetails() {
-    this.router.navigate(["/cc-details"]);
-  }
-}
+export class CcModelling {}
