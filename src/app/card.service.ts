2e22eb151e2cfd8f454e9fb57c9cb0dc9879504b
madhaviku <madhavi.kulkarni180796@gmail.com>
2020-12-05 13:56:56 +0530

    1 file modified

--- src/app/card.service.ts
+++ src/app/card.service.ts
@@ -1,25 +1,25 @@
-import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs/BehaviorSubject';
-import { Observable } from 'rxjs/Observable';
+import { Injectable } from "@angular/core";
+import { BehaviorSubject } from "rxjs/BehaviorSubject";
+import { Observable } from "rxjs/Observable";
 
 @Injectable()
 export class CardService {
+  private cardDetails = new BehaviorSubject<any>([
+    {
+      cardNumber: "0000 0000 0000 0000",
+      cardName: "madhavi@xyz.com",
+      expiryDate: "Fri Dec 18 2020 00:00:00 GMT+0530 (India Standard Time)",
+      securiyCode: "000",
+      amount: "XXX"
+    }
+  ]);
 
-  private cardDetails = new BehaviorSubject<any>([{
-    cardNumber: '0000 0000 0000 0000',
-    cardName: 'madhavi@xyz.com',
-    expiryDate: 'Fri Dec 18 2020 00:00:00 GMT+0530 (India Standard Time)',
-    securiyCode: '000',
-    amount: 'XXX'
-  }]);
-
-  setpayment(data:any) {
-    console.log('set',data)
+  setpayment(data: any) {
+    console.log("set", data);
     this.cardDetails.next([data]);
   }
 
   getpayment() {
     return this.cardDetails.asObservable();
   }
-
 }
